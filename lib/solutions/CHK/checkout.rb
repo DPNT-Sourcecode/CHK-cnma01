35eb3073c103720723c7d98d604b5815e4f49ea5
sbx_user1051 <sbx_user1051@169.254.4.117>
2019-09-13 16:37:28 +0000

    Fri Sep 13 16:37:28 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -17,13 +17,14 @@ class Checkout
     b_discounts = freqs['B'] / 2 if freqs['B']
 
     cost = (freqs['A'] || 0)*50 + (freqs['B'] || 0)*30 + (freqs['C'] || 0)*20 + (freqs['D'] || 0)*15
-    cost -= 20 * a_discounts
-    cost -= 15 * b_discounts
+    cost -= 20 * a_discounts if a_discounts
+    cost -= 15 * b_discounts if b_discounts
 
     cost
   end
 
 end
+
 
 
 
