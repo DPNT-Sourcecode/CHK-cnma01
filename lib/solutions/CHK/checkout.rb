3cfc5002dc5b8d26493570bbacaee3fcf1cd4774
sbx_user1051 <sbx_user1051@169.254.4.117>
2019-09-13 16:22:28 +0000

    Fri Sep 13 16:22:28 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -2,6 +2,7 @@
 class Checkout
   def checkout(skus)
     freqs = {}
+    cost = 0
     return -1 if skus.empty?
 
     skus.split(',').each do |sku|
@@ -12,8 +13,16 @@ class Checkout
       end
     end
 
-    
+    a_discounts = freqs['A'] / 3
+    b_discounts = freqs['B'] / 2
+
+    cost = freqs['A']*50 + freqs['B']*30 + freqs['C']*20 + freqs['D']*15
+    cost -= 20*freqs['A']
+    cost -= 15*freqs['B']
+
+    cost
   end
 
 end
+
 
